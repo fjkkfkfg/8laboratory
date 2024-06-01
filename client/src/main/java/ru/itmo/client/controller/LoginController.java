eff7621f62c39a4cfa64c5b60a7fd2832f0afc9a
artem <demin.04@list.ru>
2024-06-02 01:22:26 +0300

    2.8.5 adding commands

--- client/src/main/java/ru/itmo/client/controller/LoginController.java
+++ client/src/main/java/ru/itmo/client/controller/LoginController.java
@@ -1,10 +1,7 @@
 package ru.itmo.client.controller;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import ru.itmo.client.MainApp;
 import ru.itmo.client.utility.runtime.Runner;
 
@@ -14,6 +11,7 @@ public class LoginController {
     private MainApp mainApp;
     private Runner runner;
     private ResourceBundle bundle;
+    private ComboBox<String> languageComboBox;
 
     @FXML
     private TextField usernameField;
@@ -60,3 +58,96 @@ public class LoginController {
         mainApp.showRegisterScreen(bundle);
     }
 }
+
+//package ru.itmo.client.controller;
+//
+//import javafx.fxml.FXML;
+//import javafx.scene.control.*;
+//import ru.itmo.client.MainApp;
+//import ru.itmo.client.utility.runtime.Runner;
+//
+//import java.util.Locale;
+//import java.util.ResourceBundle;
+//
+//public class LoginController {
+//    private MainApp mainApp;
+//    private Runner runner;
+//    private ResourceBundle bundle;
+//
+//    @FXML
+//    private ComboBox<String> languageComboBox;
+//    @FXML
+//    private TextField usernameField;
+//    @FXML
+//    private PasswordField passwordField;
+//    @FXML
+//    private Button loginButton;
+//    @FXML
+//    private TextArea messageOutput;
+//
+//    public void setMainApp(MainApp mainApp) {
+//        this.mainApp = mainApp;
+//    }
+//
+//    public void setBundle(ResourceBundle bundle) {
+//        this.bundle = bundle;
+//    }
+//
+//    public void setRunner(Runner runner) {
+//        this.runner = runner;
+//    }
+//
+//    @FXML
+//    private void initialize() {
+//        // Set default language to English
+//        languageComboBox.setValue("English");
+//
+//        // Add listener to the language combo box
+//        languageComboBox.setOnAction(event -> handleLanguageChange());
+//
+//        loginButton.setOnAction(event -> handleLogin());
+//    }
+//
+//    @FXML
+//    private void handleLogin() {
+//        String username = usernameField.getText();
+//        String password = passwordField.getText();
+//        Runner.ExitCode result = runner.executeLogin(username, password);
+//        if (result == Runner.ExitCode.OK) {
+//            mainApp.showMainScreen(bundle);
+//        } else {
+//            messageOutput.appendText(bundle.getString("login.failed") + "\n");
+//        }
+//    }
+//
+//    @FXML
+//    private void handleRegister() {
+//        mainApp.showRegisterScreen(bundle);
+//    }
+//
+//    @FXML
+//    private void handleLanguageChange() {
+//        String selectedLanguage = languageComboBox.getValue();
+//        Locale locale;
+//
+//        switch (selectedLanguage) {
+//            case "Русский":
+//                locale = new Locale("ru");
+//                break;
+//            case "Slovenský":
+//                locale = new Locale("sk");
+//                break;
+//            case "中文":
+//                locale = Locale.SIMPLIFIED_CHINESE;
+//                break;
+//            case "Shqip":
+//                locale = new Locale("sq");
+//                break;
+//            default:
+//                locale = new Locale("en");
+//                break;
+//        }
+//
+//       // mainApp.setLocale(locale);
+//    }
+//}
--- client/src/main/java/ru/itmo/client/controller/RootLayoutController.java
+++ client/src/main/java/ru/itmo/client/controller/RootLayoutController.java
@@ -1,15 +1,57 @@
+//package ru.itmo.client.controller;
+//
+//import javafx.fxml.FXML;
+//import ru.itmo.client.MainApp;
+//import ru.itmo.client.utility.runtime.Runner;
+//
+//import java.util.ResourceBundle;
+//
+//public class RootLayoutController {
+//    private MainApp mainApp;
+//    private Runner runner;
+//    private ResourceBundle bundle;
+//
+//    public void setMainApp(MainApp mainApp, ResourceBundle bundle) {
+//        this.mainApp = mainApp;
+//        this.bundle = bundle;
+//    }
+//
+//    public void setRunner(Runner runner) {
+//        this.runner = runner;
+//    }
+//
+//    @FXML
+//    private void handleExit() {
+//        System.exit(0);
+//    }1
+//
+//
+//
+//    @FXML
+//    private void handleAbout() {
+//        String infoMessage = mainApp.getRunner().getInfo();
+//        MainApp.showAlert("Информация о коллекции", null, infoMessage);
+//    }
+//}
+
+
 package ru.itmo.client.controller;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
 import ru.itmo.client.MainApp;
 import ru.itmo.client.utility.runtime.Runner;
 
+import java.util.Locale;
 import java.util.ResourceBundle;
 
 public class RootLayoutController {
     private MainApp mainApp;
     private Runner runner;
     private ResourceBundle bundle;
+
+    @FXML
+    private ComboBox<String> languageComboBox;
 
     public void setMainApp(MainApp mainApp, ResourceBundle bundle) {
         this.mainApp = mainApp;
@@ -18,6 +60,12 @@ public class RootLayoutController {
 
     public void setRunner(Runner runner) {
         this.runner = runner;
+    }
+
+    @FXML
+    private void initialize() {
+        // Set default language
+        languageComboBox.setValue("English");
     }
 
     @FXML
@@ -30,4 +78,37 @@ public class RootLayoutController {
         String infoMessage = mainApp.getRunner().getInfo();
         MainApp.showAlert("Информация о коллекции", null, infoMessage);
     }
+
+    @FXML
+    private void handleLanguageChange() {
+        String selectedLanguage = languageComboBox.getValue();
+        Locale locale;
+        switch (selectedLanguage) {
+            case "Русский":
+                locale = new Locale("ru");
+                break;
+            case "Slovenský":
+                locale = new Locale("sk");
+                break;
+            case "中文":
+                locale = new Locale("zh");
+                break;
+            case "Shqip":
+                locale = new Locale("al");
+                break;
+            default:
+                locale = new Locale("en");
+                break;
+        }
+        bundle = ResourceBundle.getBundle("messages", locale);
+        mainApp.setBundle(bundle);
+        if (runner.getCurrentUserId() == null) {
+            mainApp.showLoginScreen(bundle);
+        } else {
+            mainApp.showMainScreen(bundle);
+        }
+        //mainApp.showMainScreen(bundle);
+
+    }
 }
+
